32f276f4f94f02ce433da32e28ca91526a4fd87c
yuchida-tamu <yutaside1992@gmail.com>
2022-04-30 18:34:35 +0900

    implementing input and preview

--- cmd/mdeditor/main.go
+++ cmd/mdeditor/main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
-	"github.com/gonutz/wui/v2"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
+const WIDTH = 920
+const HEIGHT = 1080
+
 func main() {
-	wui.NewWindow().Show()
+
+	a := app.New()
+	window := a.NewWindow("Simple Markdown Editor")
+
+	str := binding.NewString()
+	str.Set("")
+
+	// preview field
+	richText := widget.NewRichTextFromMarkdown("")
+	richText.Resize(fyne.NewSize(WIDTH/2, HEIGHT))
+	richText.Move(fyne.NewPos(WIDTH/2, 0))
+
+	// editor area (input field)
+	entry := widget.NewEntryWithData(str)
+	entry.MultiLine = true
+	entry.TextStyle.Monospace = true
+	entry.Resize(fyne.NewSize(WIDTH/2, HEIGHT))
+	entry.OnChanged = func(s string) {
+		richText.ParseMarkdown(s)
+	}
+
+	// container
+	content := container.NewWithoutLayout(
+		entry,
+		richText,
+	)
+	content.Resize(fyne.NewSize(WIDTH, HEIGHT))
+	content.Add(layout.NewSpacer())
+
+	window.SetContent(content)
+	window.Resize(fyne.NewSize(WIDTH, HEIGHT))
+
+	window.ShowAndRun()
+
 }
